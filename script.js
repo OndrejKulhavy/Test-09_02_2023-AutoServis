20ab7c664fa959aa55198e09dca5bac54d125c0f
Ondřej Kulhavý <okulhav@gmail.com>
2023-02-09 08:01:43 +0100

    feat: local storage

--- script.js
+++ script.js
@@ -11,15 +11,26 @@ class AutoServis{
     constructor(nazev){
         this.auta = [];
         this.nazev = nazev;
+        this.load();
     }
-
     pridejAuto(auto){
         this.auta.push(auto);
+        this.save();
     }
     smazAuto(auto){
         this.auta.splice(this.auta.indexOf(auto), 1);
+        this.save();
     }
     getAuta(){
         return this.auta;
     }
+    save(){
+        localStorage.setItem(this.nazev, JSON.stringify(this.auta));
+    }
+    load(){
+        this.auta = JSON.parse(localStorage.getItem(this.nazev));
+    }
 }
+ 
+let autoServis = new AutoServis("auta");
+let auto1 = new Auto(2010, "Skoda", "Octavia", 100000);
